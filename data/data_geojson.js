b65ac70bc2407d3db6d84723b475a75388390dcd
Marthely TSIBA <marthely.tsiba@arep.fr>
2024-02-14 15:47:36 +0100

    list of gares

--- data/data_geojson.js
+++ data/data_geojson.js
@@ -1,22 +1,110 @@
 const list_of_gares = {
     bordeaux : [
         {
-            name: "Gare Bordeaux Saint-Jean",
+            name: bordeaux_iso_20min.properties.nom_gare,
+            code_uic: bordeaux_iso_20min.properties.code_uic,
+            frequentation: bordeaux_iso_20min.properties.frequentation_22,
+            population: bordeaux_iso_20min.properties.population,
+            ratio_impermabilisation: bordeaux_iso_20min.properties.ratio_impermabilisation,
+            ratio_arbres: bordeaux_iso_20min.properties.ratio_arbres,
+            ratio_vegetation_basse: bordeaux_iso_20min.properties.ratio_vegetation_basse,
+            surface: bordeaux_iso_20min.properties.surface,
+
             iso: "20 min",
             feature: bordeaux_iso_20min, 
             style: {color: "#ff7800", weight: 0.5, fillColor: "#ff7800", fillOpacity: 0.3},
         },
         {
-            name: "Gare Bordeaux Saint-Jean",
+            name: bordeaux_iso_20min.properties.nom_gare,
+            code_uic: bordeaux_iso_20min.properties.code_uic,
+            frequentation: bordeaux_iso_20min.properties.frequentation_22,
+            population: bordeaux_iso_20min.properties.population,
+            ratio_impermabilisation: bordeaux_iso_20min.properties.ratio_impermabilisation,
+            ratio_arbres: bordeaux_iso_20min.properties.ratio_arbres,
+            ratio_vegetation_basse: bordeaux_iso_20min.properties.ratio_vegetation_basse,
+            surface: bordeaux_iso_20min.properties.surface,
+            
             iso: "15 min",
             feature: bordeaux_iso_15min, 
             style: {color: "#ff7800", weight: 1, fillColor: "#ff7800", fillOpacity: 0.2},
         },
         {
-            name: "Gare Bordeaux Saint-Jean",
+            name: bordeaux_iso_20min.properties.nom_gare,
+            code_uic: bordeaux_iso_20min.properties.code_uic,
+            frequentation: bordeaux_iso_20min.properties.frequentation_22,
+            population: bordeaux_iso_20min.properties.population,
+            ratio_impermabilisation: bordeaux_iso_20min.properties.ratio_impermabilisation,
+            ratio_arbres: bordeaux_iso_20min.properties.ratio_arbres,
+            ratio_vegetation_basse: bordeaux_iso_20min.properties.ratio_vegetation_basse,
+            surface: bordeaux_iso_20min.properties.surface,
+            
             iso: "10 min",
             feature: bordeaux_iso_10min, 
             style: {color: "#ff7800", weight: 1.5, fillColor: "#ff7800", fillOpacity: 0.1},
         },
-    ]
+    ],
+    lyon : [
+        {
+            name: lyon_iso_15min.properties.nom_gare,
+            code_uic: lyon_iso_15min.properties.code_uic,
+            frequentation: lyon_iso_15min.properties.frequentation_22,
+            population: lyon_iso_15min.properties.population,
+            ratio_impermabilisation: lyon_iso_15min.properties.ratio_impermabilisation,
+            ratio_arbres: lyon_iso_15min.properties.ratio_arbres,
+            ratio_vegetation_basse: lyon_iso_15min.properties.ratio_vegetation_basse,
+            surface: lyon_iso_15min.properties.surface,
+
+            iso: "15 min",
+            feature: lyon_iso_15min, 
+            style: {color: "#ff7800", weight: 0.5, fillColor: "#ff7800", fillOpacity: 0.3},
+        },
+    ],
+    marseille : [
+        {
+            name: marseille_iso_15min.properties.nom_gare,
+            code_uic: marseille_iso_15min.properties.code_uic,
+            frequentation: marseille_iso_15min.properties.frequentation_22,
+            population: marseille_iso_15min.properties.population,
+            ratio_impermabilisation: marseille_iso_15min.properties.ratio_impermabilisation,
+            ratio_arbres: marseille_iso_15min.properties.ratio_arbres,
+            ratio_vegetation_basse: marseille_iso_15min.properties.ratio_vegetation_basse,
+            surface: marseille_iso_15min.properties.surface,
+
+            iso: "15 min",
+            feature: marseille_iso_15min, 
+            style: {color: "#ff7800", weight: 0.5, fillColor: "#ff7800", fillOpacity: 0.3},
+        },
+    ],
+    nantes : [
+        {
+            name: nantes_iso_15min.properties.nom_gare,
+            code_uic: nantes_iso_15min.properties.code_uic,
+            frequentation: nantes_iso_15min.properties.frequentation_22,
+            population: nantes_iso_15min.properties.population,
+            ratio_impermabilisation: nantes_iso_15min.properties.ratio_impermabilisation,
+            ratio_arbres: nantes_iso_15min.properties.ratio_arbres,
+            ratio_vegetation_basse: nantes_iso_15min.properties.ratio_vegetation_basse,
+            surface: nantes_iso_15min.properties.surface,
+
+            iso: "15 min",
+            feature: nantes_iso_15min, 
+            style: {color: "#ff7800", weight: 0.5, fillColor: "#ff7800", fillOpacity: 0.3},
+        },
+    ],
+    strasbourg : [
+        {
+            name: strasbourg_iso_15min.properties.nom_gare,
+            code_uic: strasbourg_iso_15min.properties.code_uic,
+            frequentation: strasbourg_iso_15min.properties.frequentation_22,
+            population: strasbourg_iso_15min.properties.population,
+            ratio_impermabilisation: strasbourg_iso_15min.properties.ratio_impermabilisation,
+            ratio_arbres: strasbourg_iso_15min.properties.ratio_arbres,
+            ratio_vegetation_basse: strasbourg_iso_15min.properties.ratio_vegetation_basse,
+            surface: strasbourg_iso_15min.properties.surface,
+
+            iso: "15 min",
+            feature: strasbourg_iso_15min, 
+            style: {color: "#ff7800", weight: 0.5, fillColor: "#ff7800", fillOpacity: 0.3},
+        },
+    ],
 }
