500bf125dfe390aad8ecefb6c690d38bd55d3bdc
Marthely TSIBA <marthely.tsiba@arep.fr>
2024-02-14 12:48:43 +0100

    list of gares

---
+++ data/data_geojson.js
@@ -0,0 +1,22 @@
+const list_of_gares = {
+    bordeaux : [
+        {
+            name: "Gare Bordeaux Saint-Jean",
+            iso: "20 min",
+            feature: bordeaux_iso_20min, 
+            style: {color: "#ff7800", weight: 0.5, fillColor: "#ff7800", fillOpacity: 0.3},
+        },
+        {
+            name: "Gare Bordeaux Saint-Jean",
+            iso: "15 min",
+            feature: bordeaux_iso_15min, 
+            style: {color: "#ff7800", weight: 1, fillColor: "#ff7800", fillOpacity: 0.2},
+        },
+        {
+            name: "Gare Bordeaux Saint-Jean",
+            iso: "10 min",
+            feature: bordeaux_iso_10min, 
+            style: {color: "#ff7800", weight: 1.5, fillColor: "#ff7800", fillOpacity: 0.1},
+        },
+    ]
+}
